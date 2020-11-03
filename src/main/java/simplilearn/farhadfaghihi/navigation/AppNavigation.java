27b1496e5a184408a02efc1335dcae851c1643ee
Farhad <ifarhadfaghihi@gmail.com>
2020-11-03 10:01:24 -0700

    added check for invalid menu option entered

--- src/main/java/simplilearn/farhadfaghihi/navigation/AppNavigation.java
+++ src/main/java/simplilearn/farhadfaghihi/navigation/AppNavigation.java
@@ -1,9 +1,11 @@
 package simplilearn.farhadfaghihi.navigation;
 
-import org.codehaus.plexus.component.configurator.converters.basic.FileConverter;
 import simplilearn.farhadfaghihi.utils.FileUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static simplilearn.farhadfaghihi.utils.Consts.*;
@@ -13,12 +15,13 @@ import static simplilearn.farhadfaghihi.utils.Consts.*;
  */
 public class AppNavigation {
 
-    private static AppNavigation instance;
+    private final Scanner scanner = new Scanner(System.in);
     private int lastSelectedOption = -1;
+    private List<Integer> validOptions = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
 
+    private static AppNavigation instance;
 
     protected AppNavigation() {
-
     }
 
     public static AppNavigation getInstance() {
@@ -34,29 +37,93 @@ public class AppNavigation {
 
         displayMainMenu();
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
+        readMenuOptionFromConsole();
+    }
 
-        switch (option) {
-            case 0: {
-                displayFarewellMessageAndExit();
+    private void readMenuOptionFromConsole() throws IOException {
+        while (lastSelectedOption != 0) {
+            String token = scanner.next();
+            if (isValidOption(token)) {
+                lastSelectedOption = Integer.parseInt(token);
+                switch (lastSelectedOption) {
+                    case 0: {
+                        displayFarewellMessageAndExit();
+                        break;
+                    }
+
+                    case 1: {
+                        // call filedao.displayallfiles
+                    }
+
+                    case 2: {
+                        displaySearchFilesMenu();
+                        break;
+                    }
+
+                    case 3: {
+                        displayAddNewFileMenu();
+                        break;
+                    }
+
+                    case 4: {
+                        displayDeleteFileMenu();
+                        break;
+                    }
+
+                    default: {
+
+                    }
+                }
+            } else {
+                displayInvalidOptionMessage();
             }
         }
     }
 
     protected void displayWelcomeMessage() throws IOException {
-        String welcomeMessage = FileUtils.readFileContent(WELCOME_MESSAGE_FILE_PATH);
-        System.out.println(welcomeMessage);
+        printFileToConsole(WELCOME_MESSAGE_FILE_PATH);
     }
 
     protected void displayMainMenu() throws IOException {
-        String mainMenu = FileUtils.readFileContent(MAIN_MENU_FILE_PATH);
-        System.out.println(mainMenu);
+        printFileToConsole(MAIN_MENU_FILE_PATH);
     }
 
     protected void displayFarewellMessageAndExit() throws IOException {
-        String farewell = FileUtils.readFileContent(FAREWELL_MESSAGE_FILE_PATH);
-        System.out.println(farewell);
+        printFileToConsole(FAREWELL_MESSAGE_FILE_PATH);
         System.exit(0);
     }
+
+    protected void displaySearchFilesMenu() throws IOException {
+        printFileToConsole(SEARCH_MENU_FILE_PATH);
+    }
+
+    protected void displayAddNewFileMenu() throws IOException {
+        printFileToConsole(ADD_MENU_FIRST_FILE_PATH);
+    }
+
+    protected void displayDeleteFileMenu() throws IOException {
+        printFileToConsole(DELETE_MENU_FILE_PATH);
+    }
+
+    protected void displayInvalidOptionMessage() throws IOException {
+        printFileToConsole(INVALID_OPTION_MESSAGE_FILE_PATH);
+    }
+
+    private void printFileToConsole(String fileName) throws IOException {
+        String mainMenu = FileUtils.readFileContent(fileName);
+        System.out.println(mainMenu);
+    }
+
+    private boolean isValidOption(String token) {
+        if (token == null)
+            return false;
+
+        try {
+            int option = Integer.parseInt(token);
+            return validOptions.contains(option);
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
 }
--- src/main/java/simplilearn/farhadfaghihi/utils/Consts.java
+++ src/main/java/simplilearn/farhadfaghihi/utils/Consts.java
@@ -1,9 +1,12 @@
 package simplilearn.farhadfaghihi.utils;
 
 public class Consts {
-
     public static final String WELCOME_MESSAGE_FILE_PATH = "src/main/resources/welcome_message.txt";
     public static final String MAIN_MENU_FILE_PATH = "src/main/resources/main_menu.txt";
+    public static final String SEARCH_MENU_FILE_PATH = "src/main/resources/search_menu.txt";
+    public static final String ADD_MENU_FIRST_FILE_PATH = "src/main/resources/add_file_menu_first.txt";
+    public static final String ADD_MENU_SECOND_FILE_PATH = "src/main/resources/add_file_menu_second.txt";
+    public static final String DELETE_MENU_FILE_PATH = "src/main/resources/delete_file_menu.txt";
     public static final String FAREWELL_MESSAGE_FILE_PATH = "src/main/resources/farewell_message.txt";
-
+    public static final String INVALID_OPTION_MESSAGE_FILE_PATH = "src/main/resources/invalid_option_message.txt";
 }
